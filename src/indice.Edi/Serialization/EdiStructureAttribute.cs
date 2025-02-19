84b8c7e130d0c483d0e0a5265a1b3af095220626
MJapdeva4302 <mbermudez@japdeva.go.cr>
2025-02-19 08:47:13 -0600

    Add properties such as Required, Dependent, Recommended, Optional, and Not Used, so that you can use those properties, as they are used in some documents.

--- src/indice.Edi/Serialization/EdiStructureAttribute.cs
+++ src/indice.Edi/Serialization/EdiStructureAttribute.cs
@@ -6,6 +6,11 @@
 public abstract class EdiStructureAttribute : EdiAttribute
 {
     private bool _Mandatory;
+    private bool _Required;
+    private bool _Dependent;
+    private bool _Recommended;
+    private bool _Optional;
+    private bool _NotUsed;
     private string _Description;
 
     /// <summary>
@@ -14,6 +19,46 @@ public abstract class EdiStructureAttribute : EdiAttribute
     public bool Mandatory {
         get { return _Mandatory; }
         set { _Mandatory = value; }
+    }
+
+    /// <summary>
+    /// Indicates that the current structure (Segment or Element) is conditional but MUST be used anyway. By default this is false.
+    /// </summary>
+    public bool Required {
+        get { return _Required; }
+        set { _Required = value; }
+    }
+
+    /// <summary>
+    /// Indicates that the current structure (Segment or Element) is conditional and its use depends on some condition. This condition must be clarified in the description. By default this is false.
+    /// </summary>
+    public bool Dependent {
+        get { return _Dependent; }
+        set { _Dependent = value; }
+    }
+
+    /// <summary>
+    /// Indicates that the current structure (Segment or Element) is conditional and its use is recommended. By default this is false.
+    /// </summary>
+    public bool Recommended {
+        get { return _Recommended; }
+        set { _Recommended = value; }
+    }
+
+    /// <summary>
+    /// Indicates that the current structure (Segment or Element) is conditional and its use is optional at the discretion of the sender. By default this is false.
+    /// </summary>
+    public bool Optional {
+        get { return _Optional; }
+        set { _Optional = value; }
+    }
+
+    /// <summary>
+    /// Indicates that the current structure (Segment or Element) the segment must not be used. By default this is false.
+    /// </summary>
+    public bool NotUsed {
+        get { return _NotUsed; }
+        set { _NotUsed = value; }
     }
 
     /// <summary>
--- src/indice.Edi/Serialization/EdiValueAttribute.cs
+++ src/indice.Edi/Serialization/EdiValueAttribute.cs
@@ -8,6 +8,11 @@ public sealed class EdiValueAttribute : EdiAttribute
 {
     private Picture _picture;
     private bool _Mandatory;
+    private bool _Required;
+    private bool _Dependent;
+    private bool _Recommended;
+    private bool _Optional;
+    private bool _NotUsed;
     private string _Description;
     private string _Format;
     private string _Path;
@@ -18,6 +23,46 @@ public sealed class EdiValueAttribute : EdiAttribute
     public bool Mandatory {
         get { return _Mandatory; }
         set { _Mandatory = value; }
+    }
+
+    /// <summary>
+    /// Indicates that the current structure (Segment or Element) is conditional but MUST be used anyway. By default this is false.
+    /// </summary>
+    public bool Required {
+        get { return _Required; }
+        set { _Required = value; }
+    }
+
+    /// <summary>
+    /// Indicates that the current structure (Segment or Element) is conditional and its use depends on some condition. This condition must be clarified in the description. By default this is false.
+    /// </summary>
+    public bool Dependent {
+        get { return _Dependent; }
+        set { _Dependent = value; }
+    }
+
+    /// <summary>
+    /// Indicates that the current structure (Segment or Element) is conditional and its use is recommended. By default this is false.
+    /// </summary>
+    public bool Recommended {
+        get { return _Recommended; }
+        set { _Recommended = value; }
+    }
+
+    /// <summary>
+    /// Indicates that the current structure (Segment or Element) is conditional and its use is optional at the discretion of the sender. By default this is false.
+    /// </summary>
+    public bool Optional {
+        get { return _Optional; }
+        set { _Optional = value; }
+    }
+
+    /// <summary>
+    /// Indicates that the current structure (Segment or Element) the segment must not be used. By default this is false.
+    /// </summary>
+    public bool NotUsed {
+        get { return _NotUsed; }
+        set { _NotUsed = value; }
     }
 
     /// <summary>
